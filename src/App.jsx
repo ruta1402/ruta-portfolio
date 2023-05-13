02e6e503e9594431e8190a5fd4e637c69781f775
ruta1402 <ruta.kulkarni@somaiya.edu>
2023-05-13 13:30:32 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <main className='main'>
+      <Home/>
+      <About/>
+    </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/about/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./about.css";
+
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section__title">About Me</h2>
+        <span className="section__subtitle">My Introduction</span>
+        <div className="about__container container grid"></div>
+
+    </section>
+  )
+}
+
+export default About
